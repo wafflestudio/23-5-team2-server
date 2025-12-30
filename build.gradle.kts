dff48e1db43bcc062e5436c1a7f08d7f702a4b6b
HK Lee <69958270+tteokgook1@users.noreply.github.com>
2025-12-31 05:19:02 +0900

    ci/cd

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     kotlin("plugin.spring") version "2.2.21"
     id("org.springframework.boot") version "4.0.1"
     id("io.spring.dependency-management") version "1.1.7"
+    id("org.jlleitschuh.gradle.ktlint") version "13.1.0"
 }
 
 group = "com.wafflestudio"
