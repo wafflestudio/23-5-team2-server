c894e9fabef005a4c8a142ea3d886cd93071f822
HK Lee <69958270+tteokgook1@users.noreply.github.com>
2026-01-06 01:21:54 +0900

    feat: init tables, swagger test, fix: env

--- src/test/kotlin/com/wafflestudio/team2server/ApplicationTests.kt
+++ src/test/kotlin/com/wafflestudio/team2server/ApplicationTests.kt
@@ -1,17 +1,30 @@
 package com.wafflestudio.team2server
 
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc
 import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 import org.testcontainers.junit.jupiter.Testcontainers
 
 @SpringBootTest
 @ActiveProfiles("test")
 @Testcontainers
 @AutoConfigureMockMvc
-class ApplicationTests {
-    @Test
-    fun contextLoads() {
+class ApplicationTests
+    @Autowired
+    constructor(
+        private val mvc: MockMvc,
+    ) {
+        @Test
+        fun swagger() {
+            // swagger 정상 작동한다
+            mvc
+                .perform(
+                    get("/swagger-ui/index.html"),
+                ).andExpect(status().isOk)
+        }
     }
-}
