0c9d8a47dc5f23749c235a8c645e02a0d0d2d6a7
sayhwan <sehwan.yi06@gmail.com>
2026-01-07 02:59:11 +0900

    health3

--- src/test/kotlin/com/wafflestudio/team2server/HelathrouteTests.kt
+++ src/test/kotlin/com/wafflestudio/team2server/HelathrouteTests.kt
@@ -6,19 +6,20 @@ import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
 @SpringBootTest
 @AutoConfigureMockMvc
 class HelathrouteTests(
-    @Autowired private val mockMvc: MockMvc
-){
-@Test
-fun health_is_up() {
-    //healthroute 정상작동한다
-    mockMvc.perform(get("/actuator/health"))
-        .andExpect(status().isOk)
-        .andExpect(jsonPath("$.status").value("UP"))
-}
+    @Autowired private val mockMvc: MockMvc,
+) {
+    @Test
+    fun health_is_up() {
+        // healthroute 정상작동한다
+        mockMvc
+            .perform(get("/actuator/health"))
+            .andExpect(status().isOk)
+            .andExpect(jsonPath("$.status").value("UP"))
+    }
 }
