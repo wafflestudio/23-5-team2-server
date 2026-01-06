b12e4fe1f1d2d02df7856c648ee8c3068a149cda
sayhwan <sehwan.yi06@gmail.com>
2026-01-07 03:08:02 +0900

    health4

--- src/test/kotlin/com/wafflestudio/team2server/HelathrouteTests.kt
+++ src/test/kotlin/com/wafflestudio/team2server/HelathrouteTests.kt
@@ -4,20 +4,24 @@ import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc
+import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import org.testcontainers.junit.jupiter.Testcontainers
 
 @SpringBootTest
+@ActiveProfiles("test")
+@Testcontainers
 @AutoConfigureMockMvc
 class HelathrouteTests(
-    @Autowired private val mockMvc: MockMvc,
+    @Autowired private val mvc: MockMvc,
 ) {
     @Test
     fun health_is_up() {
         // healthroute 정상작동한다
-        mockMvc
+        mvc
             .perform(get("/actuator/health"))
             .andExpect(status().isOk)
             .andExpect(jsonPath("$.status").value("UP"))
