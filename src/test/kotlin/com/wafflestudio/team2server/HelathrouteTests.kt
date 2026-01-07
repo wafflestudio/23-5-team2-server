2937baaadd5c4455fc4a3d93a1af4bfa82d630a8
sehwanyi <sehwanyi06@snu.ac.kr>
2026-01-07 15:14:27 +0900

    ⚡헬스 라우트 구현 (#2)
    * helath1
    
    * health2
    
    * health2
    
    * health3
    
    * health4
    
    * health5
    
    ---------
    
    Co-authored-by: sayhwan <sehwan.yi06@gmail.com>

---
+++ src/test/kotlin/com/wafflestudio/team2server/HelathrouteTests.kt
@@ -0,0 +1,29 @@
+package com.wafflestudio.team2server
+
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import org.testcontainers.junit.jupiter.Testcontainers
+
+@SpringBootTest
+@ActiveProfiles("test")
+@Testcontainers
+@AutoConfigureMockMvc
+class HelathrouteTests(
+    @Autowired private val mvc: MockMvc,
+) {
+    @Test
+    fun health_is_up() {
+        // healthroute 정상작동한다
+        mvc
+            .perform(get("/actuator/health"))
+            .andExpect(status().isOk)
+            .andExpect(jsonPath("$.status").value("UP"))
+    }
+}
