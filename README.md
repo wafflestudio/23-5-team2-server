9222011fc5ac8105c7fb4d5e5701506d3243bdd2
HK Lee <69958270+tteokgook1@users.noreply.github.com>
2026-01-08 10:35:49 +0900

    feat: frontend url setting

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # 23-5-team2-server
 와플스튜디오 23.5기 2조 server
+
+# env
+
+루트 경로에 다음의 `.env` 파일을 작성합니다.
+
+```env
+GOOGLE_CLIENT_SECRET=YOUR-GOOGLE-SECRET
+```
+
+| Key Name                | Description             |
+|-------------------------|-------------------------|
+| `GOOGLE_CLIENT_SECRET`  | 구글 OAuth2 Client Secret |
--- src/main/kotlin/com/wafflestudio/team2server/config/WebConfig.kt
+++ src/main/kotlin/com/wafflestudio/team2server/config/WebConfig.kt
@@ -1,6 +1,7 @@
 package com.wafflestudio.team2server.config
 
 import com.wafflestudio.team2server.user.UserArgumentResolver
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.method.support.HandlerMethodArgumentResolver
 import org.springframework.web.servlet.config.annotation.CorsRegistry
@@ -9,6 +10,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 @Configuration
 class WebConfig(
     private val userArgumentResolver: UserArgumentResolver,
+    @Value("\${app.frontend.url}") private val frontendUrl: String,
 ) : WebMvcConfigurer {
     override fun addArgumentResolvers(resolvers: MutableList<HandlerMethodArgumentResolver>) {
         resolvers.add(userArgumentResolver)
@@ -17,8 +19,8 @@ class WebConfig(
     override fun addCorsMappings(registry: CorsRegistry) {
         registry
             .addMapping("/**")
-            .allowedOrigins("http://localhost:5173")
-            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            .allowedOrigins(frontendUrl)
+            .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
             .allowedHeaders("*")
             .allowCredentials(true)
     }
