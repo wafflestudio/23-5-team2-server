9edb482faa177acb4ef3b1437c5cfd988df8cb23
hyun-1210 <home102105@snu.ac.kr>
2026-01-19 18:31:55 +0900

    :fix: test change(메일 서버 체크 제외)

--- src/test/kotlin/com/wafflestudio/team2server/HelathrouteTests.kt
+++ src/test/kotlin/com/wafflestudio/team2server/HelathrouteTests.kt
@@ -7,7 +7,6 @@ import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers.print
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 import org.testcontainers.junit.jupiter.Testcontainers
@@ -19,12 +18,13 @@ import org.testcontainers.junit.jupiter.Testcontainers
 class HelathrouteTests(
     @Autowired private val mvc: MockMvc,
 ) {
+
     @Test
     fun health_is_up() {
         // healthroute 정상작동한다
+
         mvc
             .perform(get("/actuator/health"))
-            .andDo(print())
             .andExpect(status().isOk)
             .andExpect(jsonPath("$.status").value("UP"))
     }
